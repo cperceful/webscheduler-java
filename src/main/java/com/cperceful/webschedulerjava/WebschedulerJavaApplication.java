010b906d0bfcf454b4ed0f645b3f7067e47d42c2
cperceful <cpercef1@gmail.com>
2018-02-08 13:56:52 -0600

    Initial commit

---
+++ src/main/java/com/cperceful/webschedulerjava/WebschedulerJavaApplication.java
@@ -0,0 +1,12 @@
+package com.cperceful.webschedulerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebschedulerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebschedulerJavaApplication.class, args);
+	}
+}
